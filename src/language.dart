e05a7ae8bbd5849c85ef4cd7a910d39498dc1106
suoyukii <heydon-lee@outlook.com>
2024-11-08 15:39:10 +0800

    Update Chinese language

--- src/language.dart
+++ src/language.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 List<String> GetLanguage() {
   switch (Platform.localeName) {
     case 'zh-CN':
-      return ['清除 CozyIsland 日志', '清除中...', '清除结束，这个窗口可以关闭了'];
+      return ['清除 舒舒服服小岛时光 的日志', '清除中...', '清除结束，这个窗口可以关闭了'];
     default:
       return [
         'Clear CozyIsland Logs',
