2db80025cf8b237eabe7784dab8b685083abdd30
suoyukii <heydon-lee@outlook.com>
2024-11-09 08:50:45 +0800

    Fixed English name

--- src/language.dart
+++ src/language.dart
@@ -6,7 +6,7 @@ List<String> GetLanguage() {
       return ['清除 舒舒服服小岛时光 的日志', '清除中...', '清除结束，这个窗口可以关闭了'];
     default:
       return [
-        'Clear CozyIsland Logs',
+        'Clear Cozy Island Logs',
         'Removing...',
         'Remove is complete, the window can be closed.'
       ];
