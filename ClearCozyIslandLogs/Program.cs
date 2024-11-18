982143beb1419012adb9b62df63b09bb4ea4acff
suoyukii <heydon-lee@outlook.com>
2024-11-18 15:56:14 +0800

    first commit

---
+++ ClearCozyIslandLogs/Program.cs
@@ -0,0 +1,31 @@
+﻿using ClearCozyIslandLogs;
+
+// Load language
+var langs = Language.Get();
+Console.Title = langs[0];
+
+// Get path
+var root_path = Environment.GetEnvironmentVariable("appdata") + "../LocalLow/摸鱼工作室";
+var path = root_path + "/CozyIsland";
+Console.WriteLine(langs[1]);
+
+// Remove start
+if (Directory.Exists(path))
+{
+    var path_dir = new DirectoryInfo(path);
+    // Clear logs
+    var log_list = path_dir.GetFiles("*.log");
+    foreach (var log in log_list) log.Delete();
+    // Remove directory
+    if (path_dir.GetFiles().Length == 0 && path_dir.GetDirectories().Length == 0) path_dir.Delete();
+}
+
+if (Directory.Exists(root_path))
+{
+    var root_path_dir = new DirectoryInfo(root_path);
+    // Remove directory
+    if (root_path_dir.GetFiles().Length == 0 && root_path_dir.GetDirectories().Length == 0) root_path_dir.Delete();
+}
+
+Console.WriteLine(langs[2]);
+Console.ReadLine();
---
+++ README/zh-cn.md
@@ -0,0 +1,23 @@
+<h1 align="center">Clear Cozy Island Logs</h1>
+<h4 align="center">自动删除 舒舒服服小岛时光 的日志</h4>
+
+<p align="center">
+  <a href="https://opensource.org/licenses/MIT">
+    <img src="http://img.shields.io/badge/License-MIT-1e90ff?style=for-the-badge"/>
+  </a>
+  <a href="https://dotnet.microsoft.com">
+    <img src="http://img.shields.io/badge/.NET-9.0-1e90ff?style=for-the-badge"/>
+  </a>
+</p>
+
+---
+
+中文名：清除 舒舒服服小岛时光 的日志
+
+游戏 舒舒服服小岛时光 在游玩后会产生两个日志文件，这两个日志的文件体积会一直在增大。
+
+打开 ClearCozyIslandLogs，就会自动删除 舒舒服服小岛时光 产生的日志。
+
+## ✨ 安装
+
+- [下载 ClearCozyIslandLogs](https://github.com/suoyukii/ClearCozyIslandLogs/releases)
